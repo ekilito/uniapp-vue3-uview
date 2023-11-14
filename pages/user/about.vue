a1a99630fe3b8af72a6c1f2f9134b1eee5257fcb
胡静 <810898541@qq.com>
2023-11-14 14:18:58 +0800

    feat: 个人中心

--- pages/user/about.vue
+++ pages/user/about.vue
@@ -1,9 +1,13 @@
 <template>
-	<view class="about">
-		<view class="content">
+	<view >
+		<view class="image-block">
+			<u-image width="100px" height="100px"></u-image>
 		</view>
-		<view class="content">
-		</view>
+		<u-cell-group class="block">
+			<u-cell title="基本信息" value="立式磨"></u-cell>
+			<u-cell title="版本号" value="V1.1.2"></u-cell>
+			<u-cell title="版本所有" value="立式磨"></u-cell>
+		</u-cell-group>
 	</view>
 </template>
 
@@ -11,20 +15,15 @@
 </script>
 
 <style lang="scss" scoped>
-	.about {
-		padding: 10rpx;
-
-		view {
-			margin: 5rpx 0;
-			text-indent: 50rpx;
-			padding: 10rpx 20rpx;
-			box-sizing: border-box;
-		}
-
-		.title {
-			color: $uni-main-color;
-		}
-
-		.content {}
+	.image-block{
+		display: flex;
+		justify-content: center;
+		height: 180px;
+		align-items: center;
+	}
+	.block {
+		background: $uni-white !important;
+		padding: $uni-spacing-sm;
+		margin: $uni-spacing-sm;
 	}
 </style>
